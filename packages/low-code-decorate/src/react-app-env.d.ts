e5aa7cc69fdedc369e00f9bee69128ed2ea1e549
wjp <519688153@qq.com>
2023-04-18 19:18:34 +0800

    '初始化项目'

---
+++ packages/low-code-decorate/src/react-app-env.d.ts
@@ -0,0 +1,24 @@
+/// <reference types="react-scripts" />
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+declare module '*.json' {
+  const content: any;
+  export default content;
+}
+
+
+declare module "react/jsx-runtime" {
+  export default any;
+}
